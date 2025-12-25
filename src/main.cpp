bb4fc21756191cc0dff55f232402835ef338f4cb
Ben Hanna <benpaulhanna@gmail.com>
2025-12-25 14:33:33 -0500

    Updates.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
 #include <glbinding/gl/gl.h>
 #include <glbinding/glbinding.h>
-#include <GLFW/glfw3.h>
+
 #include <iostream>
+
+using namespace gl;
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
 
 int main() {
     // Initialize GLFW
@@ -29,10 +46,25 @@ int main() {
     // Initialize glbinding
     glbinding::initialize([](const char* name) { return glfwGetProcAddress(name); });
 
+    // Set the viewport
+    glViewport(0, 0, 800, 600);
+
+    // Set a callback for window resize events
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+    
+    float vertices[] = {
+         0.0f,  0.5f, 0.0f,  // Top vertex
+        -0.5f, -0.5f, 0.0f,  // Bottom left vertex
+         0.5f, -0.5f, 0.0f   // Bottom right vertex
+    };
+
+    unsigned int VBO;
+    gl::glGenBuffers(1, &VBO);
+
     // Main loop
     while (!glfwWindowShouldClose(window)) {
-        // Poll for events (keyboard, mouse, etc.)
-        glfwPollEvents();
+        // Process input
+        processInput(window);
 
         // Clear the screen with a cornflower blue color
         glClearColor(0.39f, 0.58f, 0.93f, 1.0f);  // RGBA
@@ -40,6 +72,10 @@ int main() {
 
         // Swap buffers (show what we've drawn)
         glfwSwapBuffers(window);
+
+        // Poll for events (keyboard, mouse, etc.)
+        glfwPollEvents();
+
     }
 
     // Cleanup
